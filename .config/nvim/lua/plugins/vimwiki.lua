ba286c95b9d321bca75d5a91a80456239eec65cc
Nemo D'ACREMONT <ndacremont@enseirb-matmeca.fr>
2025-05-23 12:52:02 +0200

    feat: use markdown format in vimwiki

--- .config/nvim/lua/plugins/vimwiki.lua
+++ .config/nvim/lua/plugins/vimwiki.lua
@@ -1,3 +1,10 @@
+vim.g.vimwiki_list = {
+    {
+        path = '~/vimwiki/',  -- Change this to your desired wiki path
+        syntax = 'markdown',   -- Use markdown syntax
+        ext = '.md',          -- Use .md file extension
+    },
+}
 
 return {
     "vimwiki/vimwiki"
