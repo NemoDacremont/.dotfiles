ee799fc8263a0ac6a1b005a549a8a16034fe4331
Nemo D'ACREMONT <github@dacremont.xyz>
2025-09-24 08:54:31 +0200

    feat: add lsp configs that seems important

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
 
 require("set")
 require("remap")
+require("toc")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
@@ -28,4 +29,34 @@ opts = {
 -- require("lazy").setup(plugins, opts)
 require("lazy").setup("plugins", opts)
 
+local lspconfig = require 'lspconfig'
+local configs = require 'lspconfig/configs'
 
+if not configs.golangcilsp then
+	configs.golangcilsp = {
+		default_config = {
+			cmd = {'golangci-lint-langserver'},
+			root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
+			init_options = {
+				command = { "golangci-lint", "run", "--output.json.path", "stdout", "--show-stats=false", "--issues-exit-code=1" };
+			};
+		}
+	}
+end
+
+lspconfig.golangci_lint_ls.setup {
+	filetypes = {'go','gomod'}
+}
+
+require('lspconfig').ltex_plus.setup {
+  settings = {
+    ltex = {
+      -- language = "en"
+      -- language = "de-DE"
+      language = "fr";
+    }
+  },
+  filetypes = { "vimwiki", "markdown", "md", "pandoc", "vimwiki.markdown.pandoc", "tex" },
+  -- flags = { debounce_text_changes = 300 },
+  -- on_attach = on_attach,
+}
