f20c9e397f80260e631b4913664a66d2108f6a17
Nemo D'ACREMONT <github@dacremont.xyz>
2025-10-03 10:27:48 +0200

    feat: add more ignores and remove golangci lsp config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -32,22 +32,6 @@ require("lazy").setup("plugins", opts)
 local lspconfig = require 'lspconfig'
 local configs = require 'lspconfig/configs'
 
-if not configs.golangcilsp then
-	configs.golangcilsp = {
-		default_config = {
-			cmd = {'golangci-lint-langserver'},
-			root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
-			init_options = {
-				command = { "golangci-lint", "run", "--output.json.path", "stdout", "--show-stats=false", "--issues-exit-code=1" };
-			};
-		}
-	}
-end
-
-lspconfig.golangci_lint_ls.setup {
-	filetypes = {'go','gomod'}
-}
-
 require('lspconfig').ltex_plus.setup {
   settings = {
     ltex = {
